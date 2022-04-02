4094eafedf7bcaf22528681762b252fbb3206627
miguelgutierrezruano <miguelgutierrezruano@gmail.com>
2022-04-02 12:33:33 +0200

    Player Movement

--- Aveloncho/Assets/Scripts/Player/PlayerMovement.cs
+++ Aveloncho/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Movement var
+    private float currentSpeed;
+    [SerializeField] float speed;
+    [SerializeField] Vector2 movementDirection;
+
+    // Self references
+    [SerializeField] Rigidbody2D rb;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+    }
+
+    private void Movement()
+    {
+        // X axis control
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+            movementDirection.x = -1;
+        else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+            movementDirection.x = 1;
+        else
+            movementDirection.x = 0;
+
+        // Y axis control
+        if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W))
+            movementDirection.y = -1;
+        else if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+            movementDirection.y = 1;
+        else
+            movementDirection.y = 0;
+
+        movementDirection.Normalize();
+
+        // Speed calculation
+        currentSpeed = speed;
+
+        // Movement physics
+        rb.velocity = movementDirection * currentSpeed;
+
+
+
     }
 }
